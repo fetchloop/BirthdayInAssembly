6a3d390dc3241c1cb3160f6fe52f8c2fa253f92d
fetchloop <fetch@WINTER>
2026-06-24 23:25:40 +0200

    update

--- soft.asm
+++ soft.asm
@@ -5,86 +5,179 @@ extern printf
 extern scanf
 
 section .bss
-    birthdays resb 4800 ; 100 records x 48 bytes each
-    choice resb 1
+    birthdays resb 4800 ; 100 records x 48 bytes (32 name + 16 date)
+    choice resd 1
     count resb 1
+    edit resd 1
 
 section .data
-    ; menu string
     menu db "-=-=-=-=-=-=-=-=-=-=-=-=-", 10
-        db " A Birthday S Manager M", 10
-        db "-=-=-=-=-=-=-=-=-=-=-=-=-", 10
-        db "  1. View birthdays", 10
-        db "  2. Add birthday", 10
-        db "  3. Edit birthday", 10
-        db "-=-=-=-=-=-=-=-=-=-=-=-=-", 10
-        db "Page: ", 0
+         db " A Birthday S Manager M ", 10
+         db "-=-=-=-=-=-=-=-=-=-=-=-=-", 10
+         db "  1. View birthdays", 10
+         db "  2. Add birthday", 10
+         db "  3. Edit birthday", 10
+         db "-=-=-=-=-=-=-=-=-=-=-=-=-", 10
+         db "Page: ", 0
 
-    ; the format strings for printf and scanf
     fmt_input db "%d", 0
-
-    ; the format strings for reading name and birthday
     fmt_string db "%s", 0
+    fmt_record db "%s - %s", 10, 0
+
     prompt_name db "Enter name: ", 0
     prompt_birthday db "Enter date (YYYY-MM-DD): ", 0
+
+    fmt_entry db "%d. %s - %s", 10, 0
+    prompt_which db "Edit which?: ", 10, 0
 
 section .text
 
 global main
 
 view_birthdays:
-    ; read from exePath/data.ini
-    ret
+    sub rsp, 58h
+    mov rcx, 0
+
+    .loop:
+        movzx rbx, byte [count]
+        cmp rcx, rbx
+        jge .done
+
+        mov [rsp+40h], rcx ; save loop counter
+
+        mov rax, rcx
+        mov rbx, 48
+        mul rbx
+        lea rbx, [birthdays]
+        add rax, rbx ; rax = birthdays + (i * 48)
+
+        lea rcx, [fmt_record]
+        mov rdx, rax ; name at record base
+        lea r8, [rax+32] ; date at record base + 32
+        call printf
+
+        mov rcx, [rsp+40h] ; restore loop counter
+        inc rcx
+        jmp .loop
+
+    .done:
+        add rsp, 58h
+        ret
 
 add_birthday:
-    ; read name and birthday from console
-    sub rsp, 28h
+    sub rsp, 48h
+
+    movzx rax, byte [count]
+    mov rbx, 48
+    mul rbx
+    lea rbx, [birthdays]
+    add rax, rbx ; rax = base of current record
+
+    mov [rsp+30h], rax ; save record addr
+
     mov rcx, prompt_name
     call printf
-    add rsp, 28h
 
-    ; calculate base address of current record first
-    movzx rax, byte [count]
-    mov rbx, 48
-    mul rbx
-    add rax, birthdays ; rax = base of current record
-
-    ; call scanf to read name into current record
-    sub rsp, 28h
+    mov rax, [rsp+30h]
     mov rcx, fmt_string
-    mov rdx, birthdays
-    call scanfc
-    add rsp, 28h
+    mov rdx, rax ; read name into record base
+    call scanf
 
-    movzx rax, byte [count]
-    mov rbx, 48
-    mul rbx
-    add rax, birthdays
+    mov rax, [rsp+30h]
+    mov rcx, prompt_birthday
+    call printf
+
+    mov rax, [rsp+30h]
     add rax, 32
+    mov rcx, fmt_string
+    mov rdx, rax ; read date into record base + 32
+    call scanf
+
+    movzx rbx, byte [count]
+    inc rbx
+    mov [count], bl
+
+    add rsp, 48h
     ret
 
 edit_birthday:
-    ret
+    sub rsp, 58h
+    mov rcx, 0
+
+    .loop:
+        movzx rbx, byte [count]
+        cmp rcx, rbx
+        jge .done
+
+        mov [rsp+40h], rcx ; save loop counter
+
+        mov rax, rcx
+        mov rbx, 48
+        mul rbx
+        lea rbx, [birthdays]
+        add rax, rbx ; rax = birthdays + (i * 48)
+
+        lea rcx, [fmt_entry]
+        mov rdx, [rsp+40h] ; index
+        mov r8, rax ; name
+        lea r9, [rax+32] ; date
+        call printf
+
+        mov rcx, [rsp+40h] ; restore loop counter
+        inc rcx
+        jmp .loop
+
+    .done:
+        lea rcx, [prompt_which]
+        call printf
+
+        mov rax, [rsp+40h]
+        mov rcx, fmt_input
+        mov rdx, rax ; read name into record base
+        call scanf
+
+        ; store input number and let user modify based on their int input. 
+
+
+        add rsp, 58h
+        ret
 
 main:
-    mov rcx, menu
-
+.loop:
     sub rsp, 28h
+    mov rcx, menu
     call printf
     add rsp, 28h
 
     sub rsp, 28h
-    mov rcx, fmt_input
-    mov rdx, choice
+    lea rcx, [fmt_input]
+    lea rdx, [choice]
     call scanf
     add rsp, 28h
 
-    movzx rax, byte [choice]
+    mov eax, [choice]
     cmp rax, 1
-    je view_birthdays
+    je .view
     cmp rax, 2
-    je add_birthday
+    je .add
     cmp rax, 3
-    je edit_birthday
+    je .edit
+    jmp .loop
 
-    ret
+.view:
+    sub rsp, 28h
+    call view_birthdays
+    add rsp, 28h
+    jmp .loop
+
+.add:
+    sub rsp, 28h
+    call add_birthday
+    add rsp, 28h
+    jmp .loop
+
+.edit:
+    sub rsp, 28h
+    call edit_birthday
+    add rsp, 28h
+    jmp .loop
